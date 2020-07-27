27ffa740332ba2aaebf00bc988b9c20463a9f271
Alice Prigent <aliprigent@gmail.com>
2020-07-27 17:21:39 +0200

    add map

---
+++ client/src/components/Map.jsx
@@ -0,0 +1,8 @@
+import ReactMapboxGl from "react-mapbox-gl";
+
+
+const Map = ReactMapboxGl({
+    accessToken: "pk.eyJ1IjoiYXVkZXJpY2hvbiIsImEiOiJjazlrNm1jNDIwNHE2M25sNHU4cWU1eWE2In0.TMNE58M43hWc5WV6K89wiQ"
+  });
+
+  export default Map
