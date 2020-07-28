e6b96ea4df1acb78d8a8f2584acc4e9d1a87460c
Aude Richon <auderichon@gmail.com>
2020-07-28 17:13:25 +0200

    display profile, add phone number, display user's plants

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,11 +3,60 @@ import { Link } from "react-router-dom";
 import { withUser } from "../components/Auth/withUser";
 import "../styles/Profile.css";
 import "../styles/CardItem.css";
+import apiHandler from "../api/apiHandler";
+import Card from "../components/Card";
+
 class Profile extends Component {
+  state = {};
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.props.authContext.user._id, this.state);
+    apiHandler
+      .updateProfile(this.props.authContext.user._id, this.state)
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  handleModification = () => {
+    console.log("I've been called from the card");
+    const user = this.props.authContext.user._id;
+    apiHandler
+      .getItems(user)
+      .then((data) => {
+        this.setState({ items: data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  componentDidMount() {
+    const user = this.props.authContext.user._id;
+    console.log("===================USER", user);
+    apiHandler
+      .getItems(user)
+      .then((data) => {
+        console.log("==============DATA", data);
+        this.setState({ items: data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
     const { authContext } = this.props;
     const { user } = authContext;
-
+    console.log("================THIS.STATE", this.state);
     return (
       <div style={{ padding: "100px", fontSize: "1.25rem" }}>
         <h2 style={{ fontSize: "1.5rem", marginBottom: "10px" }}>
@@ -39,60 +88,65 @@ class Profile extends Component {
             </Link>
           </div>
 
-          <div className="user-contact">
-            <h4>Add a phone number</h4>
+          {!user.phoneNumber && (
+            <div className="user-contact">
+              <h4>Add a phone number</h4>
 
-            <form className="form">
-              <div className="form-group">
-                <label className="label" htmlFor="phoneNumber">
-                  Phone number
-                </label>
-                <input
-                  className="input"
-                  id="phoneNumber"
-                  type="text"
-                  name="phoneNumber"
-                  placeholder="Add phone number"
-                />
-              </div>
-              <button className="form__button">Add phone number</button>
-            </form>
-          </div>
-
-          {/* Break whatever is belo  */}
-          <div className="CardItem">
-            <div className="item-empty">
-              <div className="round-image">
-                <img src="/media/personal-page-empty-state.svg" alt="" />
-              </div>
-              <p>You don't have any items :(</p>
-            </div>
-          </div>
-
-          <div className="CardItem">
-            <h3>Your items</h3>
-            <div className="item">
-              <div className="round-image">
-                <img
-                  src="https://vignette.wikia.nocookie.net/simpsons/images/1/14/Ralph_Wiggum.png/revision/latest/top-crop/width/360/height/360?cb=20100704163100"
-                  alt="item"
-                />
-              </div>
-              <div className="description">
-                <h2>Name of item</h2>
-                <h4>Quantity: 1 </h4>
-                <p>Description of the item</p>
-                <div className="buttons">
-                  <span>
-                    <button className="btn-secondary">Delete</button>
-                  </span>
-                  <span>
-                    <button className="btn-primary">Edit</button>
-                  </span>
+              <form
+                className="form"
+                onChange={this.handleChange}
+                onSubmit={this.handleSubmit}
+              >
+                <div className="form-group">
+                  <label className="label" htmlFor="phoneNumber">
+                    Phone number
+                  </label>
+                  <input
+                    className="input"
+                    id="phoneNumber"
+                    type="text"
+                    name="phoneNumber"
+                    placeholder="Add phone number"
+                  />
                 </div>
+                <button className="form__button" type="submit">
+                  Add phone number
+                </button>
+              </form>
+            </div>
+          )}
+
+          {user.phoneNumber && (
+            <div>
+              <h4>Phone number:</h4>
+              <p>{user.phoneNumber}</p>
+            </div>
+          )}
+
+          {!this.state.items === 0 && (
+            <div className="CardItem">
+              <div className="item-empty">
+                <div className="round-image">
+                  <img src="/media/personal-page-empty-state.svg" alt="" />
+                </div>
+                <p>You don't have any items :(</p>
               </div>
             </div>
-          </div>
+          )}
+
+          {this.state.items && (
+            <div className="CardItem">
+              <h3>Your items</h3>
+
+              {this.state.items.map((oneItem) => (
+                <Card
+                  item={oneItem}
+                  key={oneItem._id}
+                  handleModification={this.handleModification}
+                />
+              ))}
+            </div>
+          )}
         </section>
       </div>
     );
